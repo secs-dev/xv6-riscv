0ba62d03a36185b75aed0962afd5c31d8810f0ca
vityaman <vityaman.dev@yandex.ru>
2024-09-03 15:38:45 +0300

    #5 Use a single command for apt-get
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- ci/docker/dependencies.sh
+++ ci/docker/dependencies.sh
@@ -8,25 +8,25 @@ apt-get update
 
 DEBIAN_FRONTEND="noninteractive" apt-get -y install tzdata
 
-apt-get install -y software-properties-common
-apt-get install -y wget
+apt-get install -y software-properties-common wget
 
 wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | apt-key add -
 add-apt-repository "deb http://apt.llvm.org/noble/ llvm-toolchain-noble-18 main"
 
 apt-get update
 
-apt-get install -y build-essential
-apt-get install -y gcc-riscv64-unknown-elf
-apt-get install -y binutils-riscv64-unknown-elf
-apt-get install -y gdb-multiarch
-apt-get install -y qemu-system-misc
-
-apt-get install -y vim
-apt-get install -y tmux
-apt-get install -y git
-
-apt-get install -y clangd-18
-apt-get install -y bear
+apt-get install -y \
+    build-essential \
+    gcc-riscv64-unknown-elf \
+    binutils-riscv64-unknown-elf \
+    gdb-multiarch \
+    qemu-system-misc \
+    \
+    vim \
+    tmux \
+    git \
+    \
+    clangd-18 \
+    bear
 
 apt-get clean
