dfe8366b4a96547eee4f8ce0c1e1d13cda17c03a
Ivan Turin <48864186+e1turin@users.noreply.github.com>
2024-09-23 11:44:18 +0300

    Update defs.h

--- kernel/defs.h
+++ kernel/defs.h
@@ -21,7 +21,7 @@ void            bunpin(struct buf*);
 void            consoleinit(void);
 void            consoleintr(int);
 void            consputc(int);
-
+void            consputcrandomname(int);
 // exec.c
 int             exec(char*, char**);
 
