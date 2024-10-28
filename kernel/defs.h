52e7a4f504ee57b54a622251892c02755c303c3d
DecafMango <DecafMangoITMO@yandex.ru>
2024-10-28 19:25:57 +0300

    fix codestyle

--- kernel/defs.h
+++ kernel/defs.h
@@ -199,9 +199,9 @@ struct list {
 };
 
 // list.c
-void lst_init(struct list*);
-void lst_remove(struct list*);
-void lst_push(struct list*, void *);
-void *lst_pop(struct list*);
-void lst_print(struct list*);
-int lst_empty(struct list*);
+void           lst_init(struct list*);
+void           lst_remove(struct list*);
+void           lst_push(struct list*, void *);
+void*          lst_pop(struct list*);
+void           lst_print(struct list*);
+int            lst_empty(struct list*);
