87b7bc1742f30079b01b209892db2a6776724e8c
DecafMango <DecafMangoITMO@yandex.ru>
2024-10-28 18:53:49 +0300

    add buddy allocator'

--- kernel/defs.h
+++ kernel/defs.h
@@ -187,3 +187,21 @@ void            virtio_disk_intr(void);
 
 // number of elements in fixed-size array
 #define NELEM(x) (sizeof(x)/sizeof((x)[0]))
+
+// buddy.c
+void           bd_init(void*,void*);
+void           bd_free(void*);
+void           *bd_malloc(uint64);
+
+struct list {
+    struct list *next;
+    struct list *prev;
+};
+
+// list.c
+void lst_init(struct list*);
+void lst_remove(struct list*);
+void lst_push(struct list*, void *);
+void *lst_pop(struct list*);
+void lst_print(struct list*);
+int lst_empty(struct list*);
