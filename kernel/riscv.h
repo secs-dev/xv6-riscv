9dd28f5e3526197b65a58306ce90248647c77777
Nickolai Zeldovich <nickolai@csail.mit.edu>
2026-08-06 08:52:30 +0200

    properly flush icache after loading user text

--- kernel/riscv.h
+++ kernel/riscv.h
@@ -364,7 +364,7 @@ static inline void
 sfence_vma()
 {
   // the zero, zero means flush all TLB entries.
-  asm volatile("sfence.vma zero, zero");
+  asm volatile("sfence.vma zero, zero" ::: "memory");
 }
 
 // fence for memory-mapped IO
@@ -372,6 +372,13 @@ static inline void
 io_fence()
 {
   asm volatile("fence iorw, iorw" ::: "memory");
+}
+
+// fence for icache
+static inline void
+icache_fence()
+{
+  asm volatile("fence.i" ::: "memory");
 }
 
 typedef uint64 pte_t;
